771b05011dd8a607477e8d49f9785f9037323295
John Ruita <w.johnruita@gmail.com>
2019-10-16 17:56:31 +0300

    Attendance: classes recyclerview

--- app/src/main/java/com/eryxlabs/fiderides/ui/attendance/adapters/ClassesRecyclerViewAdapter.java
+++ app/src/main/java/com/eryxlabs/fiderides/ui/attendance/adapters/ClassesRecyclerViewAdapter.java
@@ -2,9 +2,13 @@ package com.eryxlabs.fiderides.ui.attendance.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class ClassesRecyclerViewAdapter extends RecyclerView.Adapter<ClassesRecyclerViewAdapter.ViewHolder> {
-    public class ClassesRecyclerViewAdapter implements RecyclerView.ViewHolder {
+    class viewHolder extends RecyclerView.ViewHolder {
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
