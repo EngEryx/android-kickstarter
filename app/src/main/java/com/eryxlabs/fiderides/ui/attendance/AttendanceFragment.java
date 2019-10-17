c795f37e27ec886c8eade26fea7981bffcabad36
John Ruita <w.johnruita@gmail.com>
2019-10-17 08:52:08 +0300

    Attedance: get streams data

--- app/src/main/java/com/eryxlabs/fiderides/ui/attendance/AttendanceFragment.java
+++ app/src/main/java/com/eryxlabs/fiderides/ui/attendance/AttendanceFragment.java
@@ -1,5 +1,6 @@
 package com.eryxlabs.fiderides.ui.attendance;
 
+import android.app.ProgressDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -9,18 +10,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.eryxlabs.fiderides.R;
+import com.eryxlabs.fiderides.models.Stream;
+import com.eryxlabs.fiderides.ui.attendance.adapters.StreamsRecyclerViewAdapter;
+import com.eryxlabs.fiderides.utils.EmptyRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AttendanceFragment extends Fragment {
 
     private AttendanceViewModel attendanceViewModel;
+    private TextView textView;
+    private ProgressDialog progressDialog;
+    private EmptyRecyclerView recyclerView;
+    private LinearLayout emptyView;
+    private StreamsRecyclerViewAdapter mStreamsRecyclerViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         attendanceViewModel = ViewModelProviders.of(this).get(AttendanceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_attendance, container, false);
-        final TextView textView = root.findViewById(R.id.text_attendance);
+        textView = root.findViewById(R.id.text_attendance);
+
         attendanceViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
--- app/src/main/java/com/eryxlabs/fiderides/ui/attendance/AttendanceViewModel.java
+++ app/src/main/java/com/eryxlabs/fiderides/ui/attendance/AttendanceViewModel.java
@@ -1,14 +1,10 @@
 package com.eryxlabs.fiderides.ui.attendance;
 
-import android.app.ProgressDialog;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
-import android.widget.LinearLayout;
 
 import com.eryxlabs.fiderides.models.Stream;
-import com.eryxlabs.fiderides.ui.attendance.adapters.StreamsRecyclerViewAdapter;
-import com.eryxlabs.fiderides.utils.EmptyRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +12,7 @@ import java.util.List;
 public class AttendanceViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-    private ProgressDialog progressDialog;
-    private List<Stream> ridesList =  new ArrayList<>();
-    private EmptyRecyclerView recyclerView;
-    private LinearLayout emptyView;
-    private StreamsRecyclerViewAdapter mStreamsRecyclerViewAdapter;
+    private List<Stream> streamsList =  new ArrayList<>();
 
     public AttendanceViewModel() {
         mText = new MutableLiveData<>();
